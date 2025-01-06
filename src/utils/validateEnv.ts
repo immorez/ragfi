0da8de16a635061804e227ee9c651a1d6fae7dd1
immorez <immorez>
2025-01-06 17:39:27 +0330

    feat: add envs to validate env function

--- src/utils/validateEnv.ts
+++ src/utils/validateEnv.ts
@@ -1,8 +1,14 @@
-import { cleanEnv, port, str } from 'envalid';
+import { cleanEnv, port, str, url } from 'envalid';
 
 export const ValidateEnv = () => {
   cleanEnv(process.env, {
-    NODE_ENV: str(),
+    NODE_ENV: str({
+      choices: ['development', 'test', 'production'], // Restrict NODE_ENV to specific values
+    }),
     PORT: port(),
+    DATABASE_URL: str(), // Validates the database connection string
+    ELASTICSEARCH_URL: url(), // Ensures it's a valid URL
+    ALPHA_VANTAGE_API_KEY: str(), // Alpha Vantage API key
+    ALPHA_VANTAGE_BASE_URL: url(), // Base URL for Alpha Vantage API
   });
 };
